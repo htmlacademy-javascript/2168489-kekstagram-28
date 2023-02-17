80951acf08c42ce3028a40aa46f544d5599dbb3e
Keks <keks@htmlacademy.ru>
2023-02-17 16:37:54 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/function.js
@@ -0,0 +1,11 @@
+const getStringLength = function(symbolNumbers) {
+  if (symbolNumbers > 10) {
+    return true;
+  }
+  return false;
+};
+
+getStringLength(10);
+
+
+console.log('Скрит подключен!');
