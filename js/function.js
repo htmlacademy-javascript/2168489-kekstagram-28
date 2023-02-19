2193189c615d0ef4702c0211d0122fe402644c98
romabuzz <romabuzz@yandex.ru>
2023-02-19 14:35:30 +0300

    Продолжаем решать функции

--- js/function.js
+++ js/function.js
@@ -1,11 +1,40 @@
-const getStringLength = function(symbolNumbers) {
-  if (symbolNumbers > 10) {
+//Функция №1
+const NEW_STRING = 'aaaaaaaaaaaaaaaaaaaaaaaa';
+
+const getStringLength = (newString, maxLength) => {
+  if (newString.length > maxLength) {
     return true;
   }
   return false;
 };
 
-getStringLength(10);
+getStringLength(NEW_STRING, 20);
 
+//Функция №2
+const isPalindrome = (chekingWord) => {
+  chekingWord = chekingWord.toLowerCase();
+  return chekingWord === chekingWord.split('').reverse().join('');
+};
 
-console.log('Скрит подключен!');
+isPalindrome('тОпот');
+
+//Функция №3
+const getOnlyNumbers = (phrase) => {
+  phrase = phrase.replace(/[^0-9]/g,'');
+  return phrase;
+};
+
+getOnlyNumbers('ECMAScript 2022');
+
+//Функция №4
+const makeNewString = (str, minLength, addSymbol) => {
+  let i = 0;
+  let newSymbol = '';
+  while (str.length + newSymbol.length <= minLength) {
+    newSymbol += addSymbol[i];
+    i = i + 1 < addSymbol.length ? i + 1 : 0;
+  }
+  return newSymbol + str;
+};
+
+makeNewString('q', 4, 'we');
