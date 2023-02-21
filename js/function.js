c6b5498e42484c2839f3fe37b496f774164d380c
romabuzz <romabuzz@yandex.ru>
2023-02-21 11:17:38 +0300

    Поправил функции после лайва

--- js/function.js
+++ js/function.js
@@ -2,7 +2,7 @@
 const NEW_STRING = 'aaaaaaaaaaaaaaaaaaaaaaaa';
 
 const getStringLength = (newString, maxLength) => {
-  if (newString.length > maxLength) {
+  if (newString.length >= maxLength) {
     return true;
   }
   return false;
@@ -11,30 +11,66 @@ const getStringLength = (newString, maxLength) => {
 getStringLength(NEW_STRING, 20);
 
 //Функция №2
+/*
 const isPalindrome = (chekingWord) => {
   chekingWord = chekingWord.toLowerCase();
   return chekingWord === chekingWord.split('').reverse().join('');
+};*/
+
+const isPalidrome = (chekingWord) => {
+  const tempWord = chekingWord.toLowerCase().replaceAll(' ', '');
+  let reverseWord = '';
+  for (let i = tempWord.length - 1; i >= 0; i--) {
+    reverseWord += tempWord.at(i);
+  }
+  return tempWord === reverseWord;
 };
 
-isPalindrome('тОпот');
+isPalidrome('тОпот');
+isPalidrome('Лёша на полке клопа нашёл');
 
 //Функция №3
+/*
 const getOnlyNumbers = (phrase) => {
   phrase = phrase.replace(/[^0-9]/g,'');
   return phrase;
+};*/
+
+const getOnlyNumbers = (phrase) => {
+  let result = '';
+  for (let i = 0; i < phrase.length; i++) {
+    if (!Number.isNaN(parseInt (phrase.at(i), 10))) {
+      result += phrase.at(i);
+    }
+  }
+  return parseInt(result, 10);
 };
 
 getOnlyNumbers('ECMAScript 2022');
 
 //Функция №4
-const makeNewString = (str, minLength, addSymbol) => {
+const makeNewString = (str, minLength, pad) => {
   let i = 0;
   let newSymbol = '';
   while (str.length + newSymbol.length <= minLength) {
-    newSymbol += addSymbol[i];
-    i = i + 1 < addSymbol.length ? i + 1 : 0;
+    newSymbol += pad[i];
+    i = i + 1 < pad.length ? i + 1 : 0;
   }
   return newSymbol + str;
 };
 
 makeNewString('q', 4, 'we');
+
+const myPadStart = (str, minLength, pad) => {
+  const actualPad = minLength - str.length;
+
+  if (actualPad <= 0) {
+    return str;
+  }
+  const tempPad = pad.slice(0, actualPad % pad.length);
+  const tempRepeat = pad.repeat(actualPad / pad.length);
+  return tempPad + tempRepeat + str;
+};
+
+myPadStart('qwerty', 4, '0');
+
