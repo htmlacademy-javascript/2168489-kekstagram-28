4a10734aa2f57d9a5a730d0d382bf27a90e2f967
Keks <keks@htmlacademy.ru>
2023-03-02 05:04:53 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -4,77 +4,34 @@ const MAX_NUMBERS_OF_LIKES = 200;
 const MAX_COMMENTS = 100;
 const MIN_AVATAR_ID = 1;
 const MAX_AVATAR_ID = 6;
-const description = ['Отличный вид', 'Вернуться бы еще раз сюда', 'Я это съел', 'Мои друзья', 'Посиделки'];
+const descriptions = ['Отличный вид', 'Вернуться бы еще раз сюда', 'Я это съел', 'Мои друзья', 'Посиделки'];
 const messages = ['В целом всё неплохо. Но не всё.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.'];
 const names = ['Михаил', 'Иван', 'Сергей', 'Татьяна', 'Мария', 'Ольга', 'Ярослава'];
 
-// функция для генерации id
-const getUniqueId = (numberOfId) => {
-  const idArray = [];
-  for (let i = 1; i <= numberOfId; i++) {
-    idArray.push(i);
-  }
-  return idArray;
-};
-const id = getUniqueId(NUMBER_OF_POSTS);
+const getDescription = () => (Math.floor(Math.random() * descriptions.length - 1));
+const getLikes = () => Math.floor(Math.random() * (MAX_NUMBERS_OF_LIKES - MIN_NUMBERS_OF_LIKES) + MIN_NUMBERS_OF_LIKES);
+const getAvatar = () => Math.floor(Math.random() * (MAX_AVATAR_ID, MIN_AVATAR_ID) + MIN_AVATAR_ID);
+const getMessage = () => Math.floor(Math.random() * messages.lenght - 1);
+const getName = () => Math.floor(Math.random() * (names.length - 1));
 
-// функция для генерации URL
-const getUniqueUrl = (numberOfUrl) => {
-  const urlArray = [];
-  for (let i = 1; i <= numberOfUrl; i++) {
-    urlArray.push(i);
-  }
-  return urlArray;
-};
-const url = getUniqueUrl(NUMBER_OF_POSTS);
-
-
-// функция для генерации случайного количества лайков от 15 до 200
-const getLikes = () => {
-  const likesCounter = Math.floor(Math.random() * (MAX_NUMBERS_OF_LIKES, MIN_NUMBERS_OF_LIKES) + MIN_NUMBERS_OF_LIKES);
-  return likesCounter;
-};
-
-// функция для генерации id комментария
-const getCommentId = () => {
-  const commentId = Math.floor(Math.random() * MAX_COMMENTS);
-  return commentId;
-};
-
-//функция для генерации случайного описания
-const getDescription = () => {
-  const randomDescription = Math.floor(Math.random() * description.length - 1);
-  return randomDescription;
-};
-
-// функция для генерации пути до файла с аватаром
-const getAvatarId = () => {
-  const avatarId = Math.floor(Math.random() * (MAX_AVATAR_ID - MIN_AVATAR_ID) + MIN_AVATAR_ID);
-  return avatarId;
-};
-
-// функция для генерации сообщения
-const getMessage = () => {
-  const randomMessage = Math.floor(Math.random() * messages.lenght - 1);
-  return randomMessage;
-};
-
-// функция для генерации имени
-const getName = () => {
-  const randomName = Math.floor(Math.random() * names.length - 1);
-  return randomName;
-};
-
-export const createPosts = () => ({
-  id:getUniqueId(NUMBER_OF_POSTS),
-  url: getUniqueUrl(NUMBER_OF_POSTS),
+const createPhoto = (index) => ({
+  id: index + 1,
+  url: `photos/${index + 1}.jpg`,
   description: getDescription(),
   likes: getLikes(),
-  comments: {
-    id: getCommentId(MAX_COMMENTS),
-    avatar: getAvatarId(MIN_AVATAR_ID, MAX_AVATAR_ID),
-    message: getMessage(),
-    name: getName()
-  },
 });
+
+const createComment = (index) => ({
+  id: index + 1,
+  avatar: getAvatar(),
+  message: getMessage(),
+  name: getName()
+});
+
+const createPhotos = () => Array.from({length: NUMBER_OF_POSTS}, (_, index) => createPhoto(index));
+const createComments = () => Array.from({length: MAX_COMMENTS}, (_, index) => createComment(index));
+
+
+console.log(createPhotos());
+console.log(createComments());
