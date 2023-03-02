f565c878ee66c07efe1e2905d8d71a17113ef86c
Keks <keks@htmlacademy.ru>
2023-03-02 17:38:30 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -9,29 +9,37 @@ const messages = ['В целом всё неплохо. Но не всё.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.'];
 const names = ['Михаил', 'Иван', 'Сергей', 'Татьяна', 'Мария', 'Ольга', 'Ярослава'];
 
-const getDescription = () => (Math.floor(Math.random() * descriptions.length - 1));
-const getLikes = () => Math.floor(Math.random() * (MAX_NUMBERS_OF_LIKES - MIN_NUMBERS_OF_LIKES) + MIN_NUMBERS_OF_LIKES);
-const getAvatar = () => Math.floor(Math.random() * (MAX_AVATAR_ID, MIN_AVATAR_ID) + MIN_AVATAR_ID);
-const getMessage = () => Math.floor(Math.random() * messages.lenght - 1);
-const getName = () => Math.floor(Math.random() * (names.length - 1));
+const getRandomValue = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+const getDescription = () => descriptions[getRandomValue(0, descriptions.length - 1)];
+
+const getLikes = () => getRandomValue(MAX_NUMBERS_OF_LIKES, MIN_NUMBERS_OF_LIKES);
+
+const getAvatar = () => getRandomValue(MAX_AVATAR_ID, MIN_AVATAR_ID);
+
+const getMessage = () => messages[getRandomValue(0, messages.length - 1)];
+
+const getName = () => names[getRandomValue(0, names.length - 1)];
+
+const createComment = (index) => ({
+  id: index + 1,
+  avatar: `img/avatar-${getAvatar()}.svg`,
+  message: getMessage(),
+  name: getName()
+});
+
+const createComments = () => Array.from({length: MAX_COMMENTS}, (_, index) => createComment(index));
 
 const createPhoto = (index) => ({
   id: index + 1,
   url: `photos/${index + 1}.jpg`,
   description: getDescription(),
   likes: getLikes(),
-});
-
-const createComment = (index) => ({
-  id: index + 1,
-  avatar: getAvatar(),
-  message: getMessage(),
-  name: getName()
+  comment: createComment(MAX_COMMENTS),
 });
 
 const createPhotos = () => Array.from({length: NUMBER_OF_POSTS}, (_, index) => createPhoto(index));
-const createComments = () => Array.from({length: MAX_COMMENTS}, (_, index) => createComment(index));
 
 
-console.log(createPhotos());
-console.log(createComments());
+createPhotos();
+createComments();
