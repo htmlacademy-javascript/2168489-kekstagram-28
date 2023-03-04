4b4119121e24a4a936f22ba8632df25ac9f39e6f
Keks <keks@htmlacademy.ru>
2023-03-04 16:01:42 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -9,37 +9,37 @@ const messages = ['В целом всё неплохо. Но не всё.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.'];
 const names = ['Михаил', 'Иван', 'Сергей', 'Татьяна', 'Мария', 'Ольга', 'Ярослава'];
 
-const getRandomValue = (min, max) => Math.floor(Math.random() * (max - min) + min);
+let counter = 1;
+
+const getRandomValue = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 const getDescription = () => descriptions[getRandomValue(0, descriptions.length - 1)];
 
-const getLikes = () => getRandomValue(MAX_NUMBERS_OF_LIKES, MIN_NUMBERS_OF_LIKES);
+const getLikes = () => getRandomValue(MIN_NUMBERS_OF_LIKES, MAX_NUMBERS_OF_LIKES);
 
-const getAvatar = () => getRandomValue(MAX_AVATAR_ID, MIN_AVATAR_ID);
+const getAvatar = () => getRandomValue(MIN_AVATAR_ID, MAX_AVATAR_ID);
 
 const getMessage = () => messages[getRandomValue(0, messages.length - 1)];
 
 const getName = () => names[getRandomValue(0, names.length - 1)];
 
-const createComment = (index) => ({
-  id: index + 1,
+const createComment = () => ({
+  id: counter++,
   avatar: `img/avatar-${getAvatar()}.svg`,
   message: getMessage(),
   name: getName()
 });
 
-const createComments = () => Array.from({length: MAX_COMMENTS}, (_, index) => createComment(index));
+const createComments = (quantity) => Array.from({length: quantity}, (_, index) => createComment(index));
 
 const createPhoto = (index) => ({
   id: index + 1,
   url: `photos/${index + 1}.jpg`,
   description: getDescription(),
   likes: getLikes(),
-  comment: createComment(MAX_COMMENTS),
+  comments: createComments(getRandomValue(0, MAX_COMMENTS)),
 });
 
 const createPhotos = () => Array.from({length: NUMBER_OF_POSTS}, (_, index) => createPhoto(index));
 
-
 createPhotos();
-createComments();
