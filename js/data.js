d0dfc727e03132a63592a2a2b86a40d624285f7c
romabuzz <romabuzz@yandex.ru>
2023-03-09 15:42:58 +0300

    Сделана модуляция

---
+++ js/data.js
@@ -0,0 +1,35 @@
+import {
+  COUNTER,
+  MAX_COMMENTS,
+  NUMBER_OF_POSTS,
+} from './varibles.js';
+
+import {
+  getRandomValue,
+  getDescription,
+  getLikes,
+  getAvatar,
+  getMessage,
+  getName,
+} from './utils.js';
+
+const createComment = () => ({
+  id: COUNTER + 1,
+  avatar: `img/avatar-${getAvatar()}.svg`,
+  message: getMessage(),
+  name: getName()
+});
+
+const createComments = (quantity) => Array.from({length: quantity}, (_, index) => createComment(index));
+
+const createPhoto = (index) => ({
+  id: index + 1,
+  url: `photos/${index + 1}.jpg`,
+  description: getDescription(),
+  likes: getLikes(),
+  comments: createComments(getRandomValue(0, MAX_COMMENTS)),
+});
+
+const createPhotos = () => Array.from({length: NUMBER_OF_POSTS}, (_, index) => createPhoto(index));
+
+export {createPhotos};
---
+++ js/varibles.js
@@ -0,0 +1,24 @@
+const NUMBER_OF_POSTS = 25;
+const MIN_NUMBERS_OF_LIKES = 15;
+const MAX_NUMBERS_OF_LIKES = 200;
+const MAX_COMMENTS = 100;
+const MIN_AVATAR_ID = 1;
+const MAX_AVATAR_ID = 6;
+const descriptions = ['Отличный вид', 'Вернуться бы еще раз сюда', 'Я это съел', 'Мои друзья', 'Посиделки'];
+const messages = ['В целом всё неплохо. Но не всё.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.'];
+const names = ['Михаил', 'Иван', 'Сергей', 'Татьяна', 'Мария', 'Ольга', 'Ярослава'];
+const COUNTER = 1;
+
+export {
+  NUMBER_OF_POSTS,
+  MIN_NUMBERS_OF_LIKES,
+  MAX_NUMBERS_OF_LIKES,
+  MAX_COMMENTS,
+  MIN_AVATAR_ID,
+  MAX_AVATAR_ID,
+  descriptions,
+  messages,
+  names,
+  COUNTER,
+};
