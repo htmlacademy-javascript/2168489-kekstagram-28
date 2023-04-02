8b1a0b589e31e987b98dcd121b3616249f66efb1
romabuzz <romabuzz@yandex.ru>
2023-04-02 17:20:56 +0300

    Добавляет модуль api

---
+++ js/message.js
@@ -0,0 +1,22 @@
+import { ERROR_TIMEOUT } from './varibles.js';
+
+const handleGetFail = () => {
+  const errorBlock = document.createElement('div');
+  errorBlock.style.position = 'fixed';
+  errorBlock.style.top = '0';
+  errorBlock.style.left = '0';
+  errorBlock.style.width = '100%';
+  errorBlock.style.height = '60px';
+  errorBlock.style.color = 'red';
+  errorBlock.style.textAlign = 'center';
+  errorBlock.style.padding = '20px';
+  errorBlock.style.backgroundColor = 'white';
+  errorBlock.textContent = 'Произошла ошибка загрузки';
+  document.body.append(errorBlock);
+
+  setTimeout(() => {
+    errorBlock.remove();
+  }, ERROR_TIMEOUT);
+};
+
+export {handleGetFail};
